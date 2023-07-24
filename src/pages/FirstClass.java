42b25009944a994b72ef3cf7b860c68bff504922
vinayak <vinayak@gmail.com>
2023-07-24 09:39:03 +0530

    first commit

---
+++ src/pages/FirstClass.java
@@ -0,0 +1,14 @@
+package pages;
+
+
+public class FirstClass {
+	public void firstMethod()
+	{
+		System.out.println("firstMethod");
+	}
+	public void secondMethod()
+	{
+		System.out.println("secondMethod");
+	}
+	
+}
---
+++ src/testClass/FirstPage.java
@@ -0,0 +1,13 @@
+package testClass;
+
+public class FirstPage {
+
+	public class SecondClass {
+		public void secondMethod()
+		{
+			System.out.println("second method");
+		}
+
+	}
+
+}
